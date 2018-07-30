4e62daac8b4fbe1eaa10de7be951ed847eadfb4e
YouWei Zhao <zhao.uv@gmail.com>
2018-07-30 18:52:51 +0800

    simulation

--- matlabtpl.m
+++ matlabtpl.m
@@ -1,28 +1,29 @@
-% % data demo
-% TBD_projectname_xy={};
-% xx_=[-250000,-250000,250000,250000];
-% yy_=[-250000,-30000,-30000,-250000];
-% TBD_projectname_xy{end+1}={xx_,yy_};
-% xx_=[-250000,-250000,250000,250000];
-% yy_=[-25000,-15000,-15000,-25000];
-% TBD_projectname_xy{end+1}={xx_,yy_};
-% xx_=[-250000,-250000,250000,250000];
-% yy_=[-10000,10000,10000,-10000];
-% TBD_projectname_xy{end+1}={xx_,yy_};
-% xx_=[-250000,-250000,250000,250000];
-% yy_=[15000,25000,25000,15000];
-% TBD_projectname_xy{end+1}={xx_,yy_};
-% xx_=[-250000,-250000,250000,250000];
-% yy_=[30000,250000,250000,30000];
-% TBD_projectname_xy{end+1}={xx_,yy_};
-% TBD_projectname_ports=[[-250000,-20000], [250000,-20000]];
-% TBD_projectname_boxsize=[500000, 500000];
-% TBD_projectname_sweep=[4, 8, 2];
-% project_name_='TBD_projectname';
-%=====^ data ^=====v simulate v=====
 %%
+%{
+% data demo
+TBD_projectname_xy={};
+xx_=[-250000,-250000,250000,250000];
+yy_=[-250000,-30000,-30000,-250000];
+TBD_projectname_xy{end+1}={xx_,yy_};
+xx_=[-250000,-250000,250000,250000];
+yy_=[-25000,-15000,-15000,-25000];
+TBD_projectname_xy{end+1}={xx_,yy_};
+xx_=[-250000,-250000,250000,250000];
+yy_=[-10000,10000,10000,-10000];
+TBD_projectname_xy{end+1}={xx_,yy_};
+xx_=[-250000,-250000,250000,250000];
+yy_=[15000,25000,25000,15000];
+TBD_projectname_xy{end+1}={xx_,yy_};
+xx_=[-250000,-250000,250000,250000];
+yy_=[30000,250000,250000,30000];
+TBD_projectname_xy{end+1}={xx_,yy_};
+TBD_projectname_ports=[[-250000,-20000], [250000,-20000]];
+TBD_projectname_boxsize=[500000, 500000];
+TBD_projectname_sweep=[4, 8, 2];
+project_name_='TBD_projectname';
+%}
+%=====^ data ^=====v simulate v=====
 Project=SonnetProject();
-TBD_projectname_Project=Project;
 Project.saveAs([project_name_,'.son']);
 % length unit
 Project.changeLengthUnit('UM');
@@ -50,20 +51,88 @@ for ii = TBD_projectname_xy
 end
 %
 TBD_projectname_ports=TBD_projectname_ports*unitratio_;
+portnum_=0;
 for ii = 2:2:size(TBD_projectname_ports,2)
+    portnum_=portnum_+1;
     Project.addPortAtLocation(TBD_projectname_ports(ii-1)+offset_(1),TBD_projectname_ports(ii)+offset_(2));
 end
-%
-Project.saveAs([project_name_,'.son']);
-% Project.openInSonnet();
 %
 Project.addSimpleFrequencySweep(TBD_projectname_sweep(1),TBD_projectname_sweep(2),TBD_projectname_sweep(3));
 % Add an output file and then resimulate
 Project.addTouchstoneOutput;
+% Project.openInSonnet();
+Project.saveAs([project_name_,'.son']);
+%===================================
+%%
+fid=fopen([project_name_,'.son'],'r');
+str=fread(fid);
+fclose(fid);
+str=char(str');
+lines=split(str,sprintf('\n'));
+clines=cell(1,size(lines,1));
+for ii = 1:size(clines,2)
+    clines{ii}=char(lines(ii));
+end
+for ii = 1:size(clines,2)
+    if strcmp(clines{ii}(1:3) , 'BOX')
+        boxindex=ii;
+        break
+    end
+end
+clines=cat(2,...
+    clines{1:boxindex-1},...
+    {'MET "Al" 1 NOR INF 0 0 '},...
+    {clines{boxindex}},...
+    {...
+        '      2000 1 1 0 0 0 0 "Air"',...
+        '      500 9.3 1 0 0 0 0 "Sapphire" A 11.5 1 0 0 0 ',...
+        'TECHLAY METAL Stream10:0 <UNSPECIFIED> 10 0 ',...
+        '0 0 0 N 0 1 1 100 100 0 0 0 Y',...
+        'END',...
+        'END',...
+        'LORGN 0 1000 U '...
+    },...
+    clines{boxindex+3:end}...
+);
+for ii = boxindex:size(clines,2)
+    if size(clines{ii},2)>=3 && strcmp(clines{ii}(1:3) , 'NUM')
+        numindex=ii;
+        break
+    end
+end
+insertindexs=[];
+insertindexs(end+1)=numindex;
+for ii = numindex:size(clines,2)
+    if size(clines{ii},2)>=7 && strcmp(clines{ii}(1:7) , 'END GEO')
+        break
+    end
+    if size(clines{ii},2)>=3 && strcmp(clines{ii}(1:3) , 'END')
+        insertindexs(end+1)=ii;
+    end
+end
+insertindexs=insertindexs(1:end-1);
+templines={};
+lastindex=0;
+for ii = insertindexs
+    templines=cat(2,templines,clines(lastindex+1:ii+1),{'TLAYNAM Stream10:0 INH'});
+    lastindex=ii+1;
+end
+clines=cat(2,templines,clines(lastindex+1:end));
+%
+fid=fopen([project_name_,'.son'],'w');
+for ii = 1:size(clines,2)
+    fprintf(fid,'%s\n',char(clines{ii}));
+end
+fclose(fid);
+%===================================
+%%
+Project=SonnetProject([project_name_,'.son']);
+TBD_projectname_Project=Project;
 Project.simulate('-c');
 % Read Touchstone Output File for S11 and S21
-S11 = TouchstoneParser([project_name_,'.s2p'],1,1);
-S21 = TouchstoneParser([project_name_,'.s2p'],2,1);
+snpfilename_=[project_name_,'.s',num2str(portnum_),'p'];
+S11 = TouchstoneParser(snpfilename_,1,1);
+S21 = TouchstoneParser(snpfilename_,2,1);
 % Convert the S11 and S21 data to dB
 S11dB = 20*log10(abs(S11(:,2)));
 S21dB = 20*log10(abs(S21(:,2)));
